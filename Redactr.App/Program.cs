9b7e6b3f97d9da10f79a98c0f64469b979f9de31
Scott Rallya <srallya@gmail.com>
2021-03-30 03:14:05 -0400

    Add project files.

---
+++ Redactr.App/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.IO.Abstractions;
+using System.Collections.Generic;
+
+namespace Redactr
+{
+    public struct RedactData
+    {
+        public RedactData(IFileSystem fs, string fn, string rw, char rc)
+        {
+            FS = fs;
+            FN = fn;
+            RW = rw;
+            RC = rc;
+        }
+        public IFileSystem FS; // Filesystem
+        public string FN; // File name
+        public string RW; // Redacted Word
+        public char RC; // Replacement Char
+    }
+
+    public struct TraverseData
+    {
+        public TraverseData(string dn, string rw)
+        {
+            DN = dn;
+            RW = rw;
+        }
+        public string DN; // Directory Name
+        public string RW; // Redacted Word
+    }
+
+    public class Redactor
+    {
+        public static void Redact(object data)
+        {
+            RedactData rd = (RedactData)data;
+            String str = rd.FS.File.ReadAllText(rd.FN);
+            str = str.Replace(rd.RW, new String(rd.RC, rd.RW.Length));
+            rd.FS.File.WriteAllText(rd.FN, str);
+        }
+    }
+
+    public class Redactr
+    {
+        public static List<Task> tasks = new List<Task>();
+
+        readonly IFileSystem filesystem;
+
+        public Redactr(IFileSystem filesystem)
+        {
+            this.filesystem = filesystem;
+        }
+
+        public Redactr() : this(
+            filesystem : new FileSystem()
+        )
+        {
+
+        }
+
+        public void TraverseDirectory(object data)
+        {
+            TraverseData td = (TraverseData)data;
+
+
+            IDirectoryInfo dirInfo = this.filesystem.DirectoryInfo.FromDirectoryName(td.DN);
+            
+            foreach (IDirectoryInfo di in dirInfo.EnumerateDirectories())
+            {
+                Redactr r = new Redactr(this.filesystem);
+                tasks.Add(Task.Factory.StartNew(() =>
+                {
+                    r.TraverseDirectory(new TraverseData(di.FullName, td.RW));
+                }));
+                
+            }
+
+            foreach (IFileInfo fi in dirInfo.EnumerateFiles())
+            {
+                tasks.Add(Task.Factory.StartNew(() =>
+                {
+                    Redactor.Redact(new RedactData(this.filesystem, fi.FullName, td.RW, '*'));
+                }));
+            }
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            if(args.Length < 2)
+            {
+                Console.WriteLine("Syntax: Redactr <Directory Name> <Word To Redact>");
+                return;
+            }
+
+            Redactr r = new Redactr();
+            r.TraverseDirectory(new TraverseData(args[0], args[1]));
+        }
+    }
+}
---
+++ Redactr.Test/RedactrTest.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using System.IO.Abstractions.TestingHelpers;
+using Redactr;
+
+namespace Redactr.Test
+{
+
+    public class RedactrTest
+    {
+        [Fact]
+        public void RedactsTextInDirectory()
+        {
+            // Arrange
+            var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData>
+            {
+                {  @"C:\Test\test.txt", new MockFileData("This is a test. This is just a test") },
+                {  @"C:\Test\Test\test.txt", new MockFileData("This is another test. Just another test.") },
+                {  @"C:\Test\Test\Test\test.txt", new MockFileData("This is the last test. One last test.") }
+            });
+
+            // Act
+            Redactr r = new Redactr(fileSystem);
+            r.TraverseDirectory(new TraverseData(@"C:\Test", "test"));
+
+            while (Redactr.tasks.Any(t => !t.IsCompleted)) { }
+
+            // Assert
+            foreach(var file in fileSystem.AllFiles)
+            {
+                var txt = fileSystem.File.ReadAllText(file);
+                Assert.DoesNotContain("test", txt);
+            }
+        }
+    }
+}
